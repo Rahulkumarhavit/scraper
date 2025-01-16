a52e0b6d9665961c667bed7593bae96cbb3e7307
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2025-01-16 23:09:05 +0530

    credits execution started

--- app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
@@ -34,6 +34,7 @@ import { ExecutionLog } from '@prisma/client';
 import { cn } from '@/lib/utils';
 import { LogLevel } from '@/types/log';
 import PhaseStatusBadge from './PhaseStatusBadge';
+import ReactCountUpWrapper from '@/components/ReactCountUpWrapper';
 
 
 type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>;
@@ -92,7 +93,7 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
 
                     <ExecutionLabel icon={ClockIcon} label="Duration" value={duration ? duration : <Loader2Icon className='animate-spin' size={20} />} />
 
-                    <ExecutionLabel icon={CoinsIcon} label="Credits consumed" value={creditsConsumed} />
+                    <ExecutionLabel icon={CoinsIcon} label="Credits consumed" value={<ReactCountUpWrapper value={creditsConsumed}/>} />
 
                 </div>
                 <Separator />
@@ -153,7 +154,7 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
                                     <CoinsIcon size={18} className='stroke-muted-foreground' />
                                     <span>Credits</span>
                                 </div>
-                                <span>Todo</span>
+                                <span>{phaseDetails.data.creditsConsumed}</span>
                             </Badge>
                             <Badge variant={"outline"} className='space-x-4'>
                                 <div className='flex gap-1 items-center'>
